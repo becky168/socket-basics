15cb596d51fc8b637069e7cc6a934525fb3c8d2c
Becky <yrstudio9316@gmail.com>
2017-01-09 15:32:46 +0800

    Added custom message event

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 // so any thing that the express app listen to, should the server listen to
 var http = require("http").Server(app);
 // this is the format that socket io expected
+// socket.io is just the way to make socket easier to use.
 var io = require("socket.io")(http);
 
 app.use(express.static(__dirname + "/public"));
@@ -14,9 +15,22 @@ app.use(express.static(__dirname + "/public"));
 // on: listen for event
 // on(eventName, callback when event happen)
 // 在有新的client連入的時候，就會執行到connection的callback function，
-// 會傳入一個socket，可以利用這個socket跟這個client溝通
-io.on("connection", function () {
+// 會傳入一個socket，可以利用這個socket跟這個client溝通 (individual connection)
+io.on("connection", function (socket) {
     console.log("User connected via socket.io!");
+
+    socket.on("message", function (message) {
+        console.log("Message received:" + message.text);
+
+        // sent the message to all of people except the person who sent the message
+        socket.broadcast.emit("message", message);
+    });
+
+    // emit an event: emit(event name, data to sent)
+    // (sent the message to all of people include the person who sent the message)
+    socket.emit("message", {
+        text: "Welcome to the chat application!"
+    });
 });
 
 // start the server
